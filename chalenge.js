4533bd6350a23cfc053f35771a43dec85740545c
nikhil-user123 <60343350+nikhil-user123@users.noreply.github.com>
2022-05-06 22:51:37 +0530

    Add files via upload

---
+++ chalenge.js
@@ -0,0 +1,27 @@
+import react from 'react';
+import reactDom from 'react-dom';
+
+
+
+const Intro=()=>{
+    return(
+
+        <>
+             
+             <h1> Nikhil Netflix </h1>
+             <p > Here the list of my five Netflix series</p>
+             <br></br>
+             <ol>
+                <li>Coffee</li>
+                <li>Tea</li>
+                <li>Milk</li>
+            </ol> 
+
+          
+        </>
+        );
+}
+
+
+
+export default Intro; 
